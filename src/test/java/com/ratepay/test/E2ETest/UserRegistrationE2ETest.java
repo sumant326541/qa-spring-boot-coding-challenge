995d7a0677e73bb7092af38be2d48d2c3198c930
Sumant Kumar <sumantkumar@airasia.com>
2023-09-11 17:27:38 +0530

    Added E2E tset

---
+++ src/test/java/com/ratepay/test/E2ETest/UserRegistrationE2ETest.java
@@ -0,0 +1,92 @@
+package com.ratepay.test.E2ETest;
+
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class UserRegistrationE2ETest {
+
+	@Test
+	public void testRegistrationWithValidData() {
+		// Prepare a valid registration request
+		String requestBody = "{\"username\": \"TestUser\", \"password\": \"Test@123\"}";
+
+		// Send a POST request to the registration endpoint
+		Response response = given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/registration");
+
+		// Assert the response
+		assertEquals(HttpStatus.CREATED.value(), response.getStatusCode()); // 201 CREATED
+
+		testValidateRegisteredUser();
+	}
+
+	public void testValidateRegisteredUser() {
+		String username = "TestUser";
+
+		// Send a GET request to the /users/{username} endpoint
+		Response response = given()
+				.when()
+				.get("/users/" + username);
+
+		// Assert the response
+		assertEquals(HttpStatus.OK.value(), response.getStatusCode()); // Assuming 200 ( User found)indicates success
+	}
+
+	@Test
+	public void testValidateNotRegisteredUser() {
+		String username = "sampleUser";
+
+		// Send a GET request to the /users/{username} endpoint
+		Response response = given()
+				.when()
+				.get("/users/" + username);
+
+		// Assert the response
+		assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode()); // Assuming 404 (User not found )indicates success
+	}
+
+	@Test
+	public void testRegistrationWithEmptyUsername() {
+		// Prepare a registration request with an empty username
+		String requestBody = "{\"username\": \"\", \"password\": \"securepassword\"}";
+
+		// Send a POST request to the registration endpoint
+		Response response = given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/registration");
+
+		// Assert the response
+		assertEquals(HttpStatus.UNPROCESSABLE_ENTITY.value(), response.getStatusCode()); // 422 "Unprocessable Entity"
+	}
+
+	@Test
+	public void testRegistrationWithEmptyPassword() {
+		// Prepare a registration request with an empty password
+		String requestBody = "{\"username\": \"validuser\", \"password\": \"\"}";
+
+		// Send a POST request to the registration endpoint
+		Response response = given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/registration");
+
+		// Assert the response
+		assertEquals(HttpStatus.UNPROCESSABLE_ENTITY.value(), response.getStatusCode()); // 422 "Unprocessable Entity"
+
+	}
+
+
+}
